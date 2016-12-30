d97b1dadf1c22e89145cec32075e50c27a49943f
Justin Johnson <justinaugustejohnson@gmail.com>
2016-12-30 16:31:49 -0500

    some weird cloning stuff

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDpqNWUTkU33m1MHRklZpuWp__QvKqkQQE",
+    authDomain: "catch-of-the-day-justin-j.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-justin-j.firebaseio.com"
+});
+
+export default base;
